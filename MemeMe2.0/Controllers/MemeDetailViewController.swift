ab3ca05a616cf7a76c7fced2dc080e1766959eab
Osmar Hernández <osmarhernandezs@gmail.com>
2020-01-20 15:06:00 -0700

    Added scenes to fit with the requirements of MemeMe 2.0

---
+++ MemeMe2.0/Controllers/MemeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe2.0
+//
+//  Created by Osmar Hernández on 20/01/20.
+//  Copyright © 2020 personal. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
